d8c4fccd64b1351a185fcd4535a6775959e85bd4
OrasanuIoana <ioanaorasanu05@gmail.com>
2024-11-28 12:18:17 +0100

    Started working on the variable elimination

--- variable_elim.py
+++ variable_elim.py
@@ -4,6 +4,9 @@
 Class for the implementation of the variable elimination algorithm.
 
 """
+from Demos.SystemParametersInfo import new_h
+
+import read_bayesnet as ReadBayesnet
 
 class VariableElimination():
 
@@ -14,6 +17,7 @@ class VariableElimination():
 
         """
         self.network = network
+
 
     def run(self, query, observed, elim_order):
         """
@@ -31,3 +35,46 @@ class VariableElimination():
                 for the query variable
 
         """
+        pass
+
+    def adjust_elimation_order(self, elim_order, query):
+        if query in elim_order:
+            elim_order.remove(query)
+
+        leaf_nodes = []
+
+        for key, value in self.network.parents:
+            if len(value) == 0:
+                leaf_nodes.append(key)
+
+        new_elimination_order = []
+        new_elimination_order.append(leaf_node for leaf_node in leaf_nodes)
+        new_elimination_order.append(elem for elem in elim_order if elem not in new_elimination_order)
+
+        # suggested by PyCharm, does not work - fix it if you feel like doing it
+        # new_elimination_oder = [leaf_node for leaf_node in leaf_nodes, elem for elem in elim_oder if
+        #                         elem not in new_elimination_oder]
+
+        return new_elimination_order
+
+    def create_factors(self):
+
+        #TODO: THIS MAY NEED TO BE UPDATED, SPECIFICALLY THE ARGUMENTS
+
+        sum_factors = []
+
+        for key, value in self.network.parents:
+            if len(value) == 0:
+                sum_factors.append(key)
+
+            else:
+                temp = value
+                temp.append(key)
+                sum_factors.extend(temp)
+
+        return sum_factors
+
+
+
+
+
